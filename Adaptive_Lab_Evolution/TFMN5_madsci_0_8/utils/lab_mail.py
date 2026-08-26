9840e4be498682cccb0f1a89933254696e51f52e
abestroka <abestroka@gmail.com>
2026-08-26 14:42:30 -0500

    Update lab_mail.py

--- Adaptive_Lab_Evolution/TFMN5_madsci_0_8/utils/lab_mail.py
+++ Adaptive_Lab_Evolution/TFMN5_madsci_0_8/utils/lab_mail.py
@@ -1,26 +1,28 @@
 import smtplib
 from email.message import EmailMessage
 
-# 1. Configure configuration and credentials
 SMTP_SERVER = "smtp.gmail.com"  
 SMTP_PORT = 465                 
 SENDER_EMAIL = "446automatedlab@gmail.com"
 SENDER_PASSWORD = "xufo ddhy gnob oqcf"
 RECEIVER_EMAIL = "cstone@anl.gov"
 
-# 2. Construct the email message
-msg = EmailMessage()
-msg["Subject"] = "TESTING"
-msg["From"] = SENDER_EMAIL
-msg["To"] = RECEIVER_EMAIL
-msg.set_content("HELLO THIS IS ABE I AM TESTING THE EMAIL SCRIPT")
 
-# 3. Connect to the server and send
-try:
-    # Use SMTP_SSL for port 465
-    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
-        server.login(SENDER_EMAIL, SENDER_PASSWORD)
-        server.send_message(msg)
-    print("Email sent successfully!")
-except Exception as e:
-    print(f"Failed to send email: {e}")
+
+
+def send_email(error_msg: str):
+    msg = EmailMessage()
+    msg["Subject"] = "446 ERROR"
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = RECEIVER_EMAIL
+    msg.set_content(error_msg)
+
+    try:
+        with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.send_message(msg)
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Failed to send email: {e}")
+
+
